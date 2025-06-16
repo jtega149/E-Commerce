98da48ea8263a6271f86f7eb4ccb007105f35bf5
John Ortega <j.tega149@gmail.com>
2025-06-16 16:07:20 -0400

    first commit

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './DescriptionBox.css';
+
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem, architecto accusantium vero eaque fuga dolores similique eos error a, aliquam commodi voluptate tempora odio laudantium atque odit enim autem doloremque.</p>
+            <p> gbysfhnusfdnudshnf fdhunwcnjwjicicwj fuencjsniiajijac veijvjisi
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox;
